ee5150a85880d6c65050ae049778865f4bf33028
sheppardjm <jsheppard@neucadia.com>
2024-08-15 12:23:09 -0400

    Add zudstand store for onboarding bool

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,12 +1,20 @@
 import { Button, View, StyleSheet } from "react-native";
 import { theme } from "@/theme";
+import { useRouter } from "expo-router";
 import { useUserStore } from "@/store/userStore";
 
-export default function ProfileScreen() {
+export default function OnboardingScreen() {
+  const router = useRouter();
   const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
+
+  const handlePress = () => {
+    toggleHasOnboarded();
+    router.navigate("/");
+  };
+
   return (
     <View style={styles.container}>
-      <Button title="Back to onboarding" onPress={toggleHasOnboarded} />
+      <Button title="Let me in!" onPress={handlePress} />
     </View>
   );
 }
