57b6374f20ade213297aa419d5c0b240fbc0745e
sheppardjm <jsheppard@neucadia.com>
2024-08-15 14:06:41 -0400

    Add plantcard and implement store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
+import { usePlantStore } from "@/store/plantsStore";
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { useRouter } from "expo-router";
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore((state) => state.plants);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={plants}
+        renderItem={({ item }) => <PlantCard plant={item} />}
+        ListEmptyComponent={
+          <PlantlyButton
+            title="Add a plant"
+            onPress={() => {
+              router.navigate("/new");
+            }}
+          />
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
